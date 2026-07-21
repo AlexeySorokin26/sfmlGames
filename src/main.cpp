03fca48259be56de83e12bdb3dc72e92ab9384f0
Alexey Sorokin <asorokin@gremse-it.com>
2026-07-21 19:01:39 +0200

    sources

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,19 @@ using namespace sf;
 int main()
 {
 	VideoMode vm(Vector2u(1920, 1080));
-	RenderWindow window(vm, "Timber!", State::Fullscreen);
+	RenderWindow window(vm, "Timber!", State::Windowed);
+
+	Texture textureBackground;
+	textureBackground.loadFromFile("resources/graphics/background.png");
+	Sprite spriteBackground(textureBackground);
+	spriteBackground.setPosition(Vector2f(0, 0));
 
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) {
 			window.close();
 		}
 		window.clear();
+		window.draw(spriteBackground);
 		window.display();
 	}
 
